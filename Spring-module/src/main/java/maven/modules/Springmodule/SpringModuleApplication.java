f9af25e84a2a4c82a714d1cdf396f76a68fb4e94
Tamas Hanicz <hanicz20@gmail.com>
2023-11-19 20:10:22 +0100

    cont

---
+++ Spring-module/src/main/java/maven/modules/Springmodule/SpringModuleApplication.java
@@ -0,0 +1,13 @@
+package maven.modules.Springmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModuleApplication.class, args);
+	}
+
+}
---
+++ Spring-module/src/test/java/maven/modules/Springmodule/SpringModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package maven.modules.Springmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
